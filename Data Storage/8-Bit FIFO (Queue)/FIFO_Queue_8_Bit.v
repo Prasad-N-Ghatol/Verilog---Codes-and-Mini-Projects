3189a16196217a3bd51fb048021c2bdfc81eb169
Prasad N Ghatol <prasadnghatol@gmail.com>
2024-05-14 20:02:55 +0530

    Added "8-Bit FIFO (Queue)" to the "Data Storage" Directory.

---
+++ Data Storage/8-Bit FIFO (Queue)/FIFO_Queue_8_Bit.v
@@ -0,0 +1,95 @@
+/*
+Verilog Code, to implement a 8-Bit FIFO (Queue).
+
+
+
+Author : Prasad Narayan Ghatol
+*/
+
+
+
+module FIFO_Queue_8_Bit (
+    input            Clk_In,
+    input            Reset_In,
+
+    input      [7:0] Data_In,
+    output reg [7:0] Data_Out,
+    input            Write_Enable_In,
+    input            Read_Enable_In,
+
+    output           FIFO_Empty,
+    output           FIFO_Full
+);
+
+
+
+// --------------------------------------------------
+// wires and regs
+// --------------------------------------------------
+reg [3:0] Write_Pointer;
+reg [3:0] Read_Pointer;
+
+
+
+// --------------------------------------------------
+// FIFO Memory
+// --------------------------------------------------
+reg [7:0] FIFO_MEMORY [7:0];
+
+
+
+// --------------------------------------------------
+// Assignments
+// --------------------------------------------------
+assign FIFO_Empty = (Write_Pointer == Read_Pointer);
+
+assign FIFO_Full = ({~Write_Pointer[3], Write_Pointer[2:0]} == Read_Pointer);
+
+
+
+// --------------------------------------------------
+// FIFO Logic
+// --------------------------------------------------
+// FIFO Write
+always @ (negedge Clk_In or posedge Reset_In)
+    begin
+        if (Reset_In)
+            begin
+                Write_Pointer <= 4'b0;
+            end
+        else if (Write_Enable_In && ~FIFO_Full)
+            begin
+                FIFO_MEMORY[Write_Pointer] <= Data_In;
+                Write_Pointer <= Write_Pointer + 1'b1;
+            end
+        else
+            begin
+                Write_Pointer <= Write_Pointer;
+            end
+    end
+
+
+
+// FIFO Read
+always @ (negedge Clk_In or posedge Reset_In)
+    begin
+        if (Reset_In)
+            begin
+                Read_Pointer <= 4'b0;
+                Data_Out <= 8'bZ;
+            end
+        else if (Read_Enable_In && ~FIFO_Empty)
+            begin
+                Data_Out <= FIFO_MEMORY[Read_Pointer];
+                Read_Pointer <= Read_Pointer + 1'b1;
+            end
+        else
+            begin
+                Read_Pointer <= Read_Pointer;
+                Data_Out <= 8'bZ;
+            end
+    end
+
+
+
+endmodule
---
+++ Data Storage/8-Bit FIFO (Queue)/FIFO_Queue_8_Bit_tb.sv
@@ -0,0 +1,109 @@
+/*
+SystemVerilog Testbench for the 8-Bit FIFO (Queue).
+
+
+
+Author : Prasad Narayan Ghatol
+*/
+
+
+
+module FIFO_Queue_8_Bit_tb ();
+
+
+
+reg        Clk_In;
+reg        Reset_In;
+
+reg  [7:0] Data_In;
+wire [7:0] Data_Out;
+reg        Write_Enable_In;
+reg        Read_Enable_In;
+
+wire       FIFO_Empty;
+wire       FIFO_Full;
+
+
+
+// --------------------------------------------------
+// FIFO DUT Instantiation
+// --------------------------------------------------
+FIFO_Queue_8_Bit DUT (
+    .Clk_In(Clk_In),
+    .Reset_In(Reset_In),
+
+    .Data_In(Data_In),
+    .Data_Out(Data_Out),
+    .Write_Enable_In(Write_Enable_In),
+    .Read_Enable_In(Read_Enable_In),
+
+    .FIFO_Empty(FIFO_Empty),
+    .FIFO_Full(FIFO_Full)
+);
+
+
+
+// --------------------------------------------------
+// Clock Initialization
+// --------------------------------------------------
+initial
+    begin
+        Clk_In = 1'b0;
+
+        forever
+            begin
+                #5;
+                Clk_In = ~Clk_In;
+            end
+    end
+
+
+
+// --------------------------------------------------
+// Testbench Logic
+// --------------------------------------------------
+    initial
+    begin
+        Reset_In = 1'b1;
+        #10;
+        Reset_In = 1'b0;
+
+        // Read from an empty FIFO
+        Write_Enable_In = 1'b0;
+        Read_Enable_In = 1'b1;
+        #10;
+
+        // Write 8 Bytes of Data into FIFO
+        repeat (8)
+            begin
+                Data_In = $random;
+                Write_Enable_In = 1'b1;
+                Read_Enable_In = 1'b0;
+                #10;
+            end
+
+        // Write into a full FIFO
+        Data_In = $random;
+        Write_Enable_In = 1'b1;
+        Read_Enable_In = 1'b0;
+        #10;
+
+        // Read 8 Bytes of Data from FIFO
+        repeat (8)
+            begin
+                Write_Enable_In = 1'b0;
+                Read_Enable_In = 1'b1;
+                #10;
+            end
+
+        // Read from an empty FIFO
+            Write_Enable_In = 1'b0;
+            Read_Enable_In = 1'b1;
+            #10;
+    
+        $stop;
+    end
+
+
+
+endmodule
