0295a8a5cce77aa5f7aaccd5fb15a25ba0e6f79b
Prasad N Ghatol <prasadnghatol@gmail.com>
2024-05-15 19:57:46 +0530

    Added "Half Adder" to the "Adders and Subtractors" Directory.

---
+++ Adders and Subtractors/Half Adder/Half_Adder.v
@@ -0,0 +1,30 @@
+/*
+Verilog Code, to implement a Half Adder.
+
+
+
+Author : Prasad Narayan Ghatol
+*/
+
+
+
+module Half_Adder (
+    input  Data_A_In,
+    input  Data_B_In,
+
+    output Sum_Out,
+    output Carry_Out
+);
+
+
+
+// --------------------------------------------------
+// Assignments
+// --------------------------------------------------
+assign Sum_Out = Data_A_In ^ Data_B_In;
+
+assign Carry_Out = Data_A_In & Data_B_In;
+
+
+
+endmodule
---
+++ Adders and Subtractors/Half Adder/Half_Adder_tb.sv
@@ -0,0 +1,54 @@
+/*
+SystemVerilog Testbench for the Half Adder.
+
+
+
+Author : Prasad Narayan Ghatol
+*/
+`timescale 1ns/1ps
+
+
+
+module Half_Adder_tb ();
+
+
+
+reg  Data_A_In;
+reg  Data_B_In;
+
+wire Sum_Out;
+wire Carry_Out;
+
+
+
+// --------------------------------------------------
+// Half Adder DUT Instantiation 
+// --------------------------------------------------
+Half_Adder DUT (
+    .Data_A_In(Data_A_In),
+    .Data_B_In(Data_B_In),
+    
+    .Sum_Out(Sum_Out),
+    .Carry_Out(Carry_Out)
+);
+    
+
+
+// --------------------------------------------------
+// Testbench Logic
+// --------------------------------------------------
+initial
+    begin
+        repeat (20)
+            begin
+                Data_A_In = $random;
+                Data_B_In = $random;
+                #10;
+            end
+        
+        $stop;
+    end
+
+
+
+endmodule
