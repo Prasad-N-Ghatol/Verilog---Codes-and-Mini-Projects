174482463b83418057647c83dadf80e0a560523e
Prasad N Ghatol <prasadnghatol@gmail.com>
2024-06-16 11:40:26 +0530

    Updated the Comparator Codes, to handle 1, 2, 4, 8, 16 and 32 input values.

---
+++ Arithmetic and Logic Modules/Comparator - 32-Bit/Comparator_32_Bit_tb.sv
@@ -0,0 +1,60 @@
+/*
+SystemVerilog Testbench for the Comparator - 32-Bit.
+
+
+
+Author : Prasad Narayan Ghatol
+*/
+`timescale 1ns/1ps
+
+
+
+module Comparator_32_Bit_tb ();
+
+
+reg         Reset_In;
+
+reg  [31:0] Data_A_In;
+reg  [31:0] Data_B_In;
+wire        A_Less_Than_B_Out;
+wire        A_Equal_To_B_Out;
+wire        A_Greater_Than_B_Out;
+
+
+
+// --------------------------------------------------
+// Comparator - 32-Bit DUT Instantiation
+// --------------------------------------------------
+Comparator_32_Bit DUT (
+    .Reset_In(Reset_In),
+    .Data_A_In(Data_A_In),
+    .Data_B_In(Data_B_In),
+    .A_Less_Than_B_Out(A_Less_Than_B_Out),
+    .A_Equal_To_B_Out(A_Equal_To_B_Out),
+    .A_Greater_Than_B_Out(A_Greater_Than_B_Out)
+);
+
+
+
+// --------------------------------------------------
+// Testbench Logic
+// --------------------------------------------------
+initial
+    begin
+        Reset_In = 1'b1;
+        #10;
+        Reset_In = 1'b0;
+
+        repeat (10)
+            begin
+                Data_A_In = $random;
+                Data_B_In = $random;
+                #10;
+            end
+
+        $stop;
+    end
+
+
+
+endmodule
---
+++ Arithmetic and Logic Modules/Comparator - 8-Bit/Comparator_8_Bit.v
@@ -0,0 +1,62 @@
+/*
+Verilog Code, to implement a Comparator - 8-Bit.
+
+
+
+Author : Prasad Narayan Ghatol
+*/
+
+
+
+module Comparator_8_Bit (
+    input            Reset_In,
+
+    input      [7:0] Data_A_In,
+    input      [7:0] Data_B_In,
+    output reg       A_Less_Than_B_Out,
+    output reg       A_Equal_To_B_Out,
+    output reg       A_Greater_Than_B_Out
+);
+
+
+
+// --------------------------------------------------
+// Comparator - 8-Bit Logic
+// --------------------------------------------------
+always @ (*)
+    begin
+        if (Reset_In)
+            begin
+                A_Less_Than_B_Out <= 1'bZ;
+                A_Equal_To_B_Out <= 1'bZ;
+                A_Greater_Than_B_Out <= 1'bZ;
+            end
+        else if (Data_A_In < Data_B_In)
+            begin
+                A_Less_Than_B_Out <= 1'b1;
+                A_Equal_To_B_Out <= 1'b0;
+                A_Greater_Than_B_Out <= 1'b0;
+            end
+        else if (Data_A_In == Data_B_In)
+            begin
+                A_Less_Than_B_Out <= 1'b0;
+                A_Equal_To_B_Out <= 1'b1;
+                A_Greater_Than_B_Out <= 1'b0;
+            end
+        else if (Data_A_In > Data_B_In)
+            begin
+                A_Less_Than_B_Out <= 1'b0;
+                A_Equal_To_B_Out <= 1'b0;
+                A_Greater_Than_B_Out <= 1'b1;
+            end
+        else
+            begin
+                A_Less_Than_B_Out <= 1'bZ;
+                A_Equal_To_B_Out <= 1'bZ;
+                A_Greater_Than_B_Out <= 1'bZ;
+            end
+    end
+
+
+
+endmodule
